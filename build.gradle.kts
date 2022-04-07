72e63b4c6350f33f62e5964a9d0e2d749b0917a3
botlikes456 <75356972+botlikes456@users.noreply.github.com>
2022-04-07 17:55:45 +0800

    initialize

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,64 +1,65 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+	kotlin("jvm") version "1.6.20"
 }
 
 allprojects {
-    group = "com.matrdata"
-    version = "1.0.0-SNAPSHOT"
+	group = "com.matrdata"
+	version = "1.0.0-SNAPSHOT"
 
-    apply {
-        plugin("java")
-    }
+	apply {
+		plugin("java")
+	}
 
-    repositories {
-        mavenCentral()
-    }
+	repositories {
+		mavenCentral()
+	}
 
-    java {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+	java {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
 }
 
 subprojects {
-    group = "com.matrdata"
-    version = "1.0.0-SNAPSHOT"
+	group = "com.matrdata"
+	version = "1.0.0-SNAPSHOT"
 
-    configure<SourceSetContainer> {
-        named("main") {
-            java.srcDir("src/main/kotlin")
-        }
-    }
+	configure<SourceSetContainer> {
+		named("main") {
+			java.srcDir("src/main/kotlin")
+		}
+	}
 
-    repositories {
-        mavenCentral()
-    }
+	repositories {
+		mavenCentral()
+	}
 
-    apply {
-        plugin("kotlin")
-        plugin("java")
-    }
+	apply {
+		plugin("kotlin")
+		plugin("java")
+	}
 
-    dependencies {
-        implementation(kotlin("stdlib-jdk8"))
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    }
+	dependencies {
+		implementation(kotlin("stdlib-jdk8"))
+		implementation("org.slf4j:slf4j-api:2.0.0-alpha")
+		testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+		testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+	}
 
-    tasks.getByName<Test>("test") {
-        useJUnitPlatform()
-    }
+	tasks.getByName<Test>("test") {
+		useJUnitPlatform()
+	}
 
-    val compileKotlin: KotlinCompile by tasks
-    compileKotlin.kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    val compileTestKotlin: KotlinCompile by tasks
-    compileTestKotlin.kotlinOptions {
-        jvmTarget = "1.8"
-    }
+	val compileKotlin: KotlinCompile by tasks
+	compileKotlin.kotlinOptions {
+		jvmTarget = "1.8"
+	}
+	val compileTestKotlin: KotlinCompile by tasks
+	compileTestKotlin.kotlinOptions {
+		jvmTarget = "1.8"
+	}
 }
 
 
